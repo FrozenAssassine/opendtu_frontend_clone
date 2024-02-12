4911bc806ce194e6e2cdecc728fc3cd403b875e7
Julius Kirsch <Julius@skirsch.de>
2024-02-12 16:43:40 +0100

    feat: navigationbar, live, history and table page

---
+++ src/components/tablePage/TablePage.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { SolarData, loadAllData } from "../../dataParser";
+import { DailySolarData } from "../solarGraphPage/solarGraphPage";
+import styles from "./TablePage.module.scss";
+
+export default function TablePage() {
+    const [solarData, setSolarData] = useState<DailySolarData[]>([]);
+
+    const fetchData = async () => {
+        await loadAllData().then((data) => {
+            let items: DailySolarData[] = [];
+
+            let lines: string[] = data.split("\r\n");
+
+            for (let i = 0; i < lines.length; i++) {
+                let separated = lines[i].split("|");
+                items.push(
+                    new DailySolarData(
+                        separated[0],
+                        parseFloat(separated[1]),
+                        parseFloat(separated[2]),
+                        parseFloat(separated[3]),
+                        separated[4],
+                        parseFloat(separated[5]),
+                        separated[6]
+                    )
+                );
+            }
+            setSolarData(items.reverse());
+        });
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className={styles.tablePage}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tag</th>
+                        <th>KW (Insgesamt)</th>
+                        <th>Watt (Täglich)</th>
+                        <th>Watt (Peak)</th>
+                        <th>Temperatur (Peak)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {solarData.map((day, index) => (
+                        <tr key={index}>
+                            <td>{day.Date}</td>
+                            <td>{day.YieldTotal} KWh</td>
+                            <td>{day.YieldDay} W</td>
+                            <td>{day.HighestWatt} Wh</td>
+                            <td>{day.Temperature} °C</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
 import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
-import SolarGraphPage from './components/solarGraphPage/SolarGraphPage';
+import SolarGraphPage from './components/solarGraphPage/solarGraphPage';
 import HomePage from './components/homePage/HomePage';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (<HomePage/>),
+    element: (<App activeIndex={0}/>),
   },
   {
     path: "*",
     element: (<div>404040040404040404040404 LOL</div>),
   },
   {
-    path: "/stats",
-    element: (<SolarGraphPage/>)
+    path: "/live",
+    element: (<App activeIndex={0}/>),
+  },  {
+    path: "/history",
+    element: (<App activeIndex={1}/>),
+  },  {
+    path: "/table",
+    element: (<App activeIndex={2}/>),
   },
 ]);
 createRoot(document.getElementById("root") as HTMLElement).render(
